bfe3f805ede54f15ab997ebb7b49b34f802b8d3a
abhinay5993 <abhinaylunawat5993@gmail.com>
2025-06-28 14:00:48 +0530

    updated the TestThon initital code.

---
+++ src/test/java/hy/testhon/hytest_autoui/HytestAutouiApplicationTests.java
@@ -0,0 +1,13 @@
+package hy.testhon.hytest_autoui;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+class HytestAutouiApplicationTests {
+
+	@Test
+	public void heyOneTests() {
+	System.out.println("\nWelcome to Java!!,,");
+	Assert.assertTrue(true);
+	}
+
+}
